a0d9b6faf71e4500e653305d12ccda9c88855988
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2025-05-20 17:00:15 -0500

    Changed text.

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div className="page">
       <h1 className="text-3xl font-bold mb-4">Skills</h1>
       <p className="text-lg">
-        I have acquired many different skillset through my experiences working on projects for school, clients, and personal endeavors. Below you can see some of the relevant proficiencies in technologies that I&apos;ve obtained and the ones that I&apos;m progressing towards as I ever seek to push past my limits.
+        I have acquired many different skillsets through my experiences working on projects for school, clients, and personal endeavors. Below you can see some of the relevant proficiencies in technologies that I&apos;ve obtained and the ones that I&apos;m progressing towards as I ever seek to push past my limits.
       </p>
 
       <SkillsPanel />
